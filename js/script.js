54d102ff2996f4b49ab52a3cfe8660cd13cf7c6f
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-07-28 17:28:49 +0000

    Added global variables at script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
+    // Set Up Global Variables
+    let gameInPlay = false;
+    let winCombos = [[1,2,3],
+                    [4,5,6],
+                    [7,8,9],
+                    [1,4,7],
+                    [2,5,8],
+                    [3,6,9],
+                    [1,5,9],
+                    [7,5,3]];
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
     
 });
