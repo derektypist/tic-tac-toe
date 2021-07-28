72aa07912fd9d75ecfc228c3f58eb705908c97d6
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-07-28 17:44:59 +0000

    Updated showGameStarter function at script.js

--- js/script.js
+++ js/script.js
@@ -11,5 +11,30 @@ $(document).ready(function() {
                     [7,5,3]];
     let playerOneScore = 0;
     let playerTwoScore = 0;
-    
+    let timeOuts = [];
+    let numFilledIn = 0;
+    let currentBoard = {1:'',2:'',3:'',4:'',5:'',6:'',7:'',8:'',9:''};
+
+    /* 
+        Display Functions
+    */
+
+    // Function to Hide Game Starter
+    function hideGameStarter() {
+        $('.game-starter').fadeOut();
+    }
+
+    // Function to Show Game Starter
+    function showGameStarter(isTwoPlayer) {
+        let message;
+        if (isTwoPlayer) {
+            message = "Player 1 : Would you like X or O?";
+        } else {
+            message = "Would you like to be X or O?"
+        }
+
+        timeOuts.push(setTimeout(function() {
+            $('.game-starter').fadeIn(500).children('p').text(message);
+        }, 700));
+    }
 });
