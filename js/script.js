3cf3e2cb5f961c9c2099fc02cee2f9bc0c028d29
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-07-29 11:14:06 +0000

    Added drawBoard Function at script.js

--- js/script.js
+++ js/script.js
@@ -126,5 +126,39 @@ $(document).ready(function() {
     }
 
     // Function to draw board
-    function drawBoard()
+    function drawBoard() {
+        timeOuts.push(setTimeout(function() {
+            let c = document.getElementById("my-canvas");
+            let canvas = c.getContext("2d");
+            canvas.lineWidth = 1;
+            canvas.strokeStyle = "#fff";
+
+            // Draw Vertical Lines
+            canvas.beginPath();
+            canvas.moveTo(100,0);
+            canvas.lineTo(100,146.5);
+            canvas.closePath();
+            canvas.stroke();
+            canvas.beginPath();
+            canvas.moveTo(200,0);
+            canvas.lineTo(200,146.5);
+            canvas.closePath();
+            canvas.stroke();
+
+            // Draw Horizontal Lines
+            canvas.lineWidth = 0.5;
+            canvas.beginPath();
+            canvas.moveTo(4,48.5);
+            canvas.lineTo(296,48.5);
+            canvas.closePath();
+            canvas.stroke();
+
+            canvas.beginPath();
+            canvas.moveTo(4,98.5);
+            canvas.lineTo(296,98.5);
+            canvas.closePath();
+            canvas.stroke();
+
+        }, 1500));
+    }
 });
